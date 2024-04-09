001a78251799992287ceef2489248209a78608b8
Dan Andrei Niculae <dan.niculae@logichainge.com>
2024-04-09 16:54:46 +0300

    jhgj

--- vue.config.js
+++ vue.config.js
@@ -2,16 +2,6 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
-    devMiddleware: {
-      publicPath: '/',
-      writeToDisk: true,
-      headers: {
-        'Access-Control-Allow-Origin': '*',
-      },
-      client: {
-        overlay: false,
-      }
-    },
     allowedHosts: 'all',
   },
 })
