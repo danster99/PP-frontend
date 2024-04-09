433b0f81e54c75b3451f41d0c9f70acd796a0229
Dan Andrei Niculae <dan.niculae@logichainge.com>
2024-04-09 16:50:03 +0300

    try fix

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,19 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
-    disableHostCheck: true,
+    devMiddleware: {
+      publicPath: '/',
+      writeToDisk: true,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+      },
+      client: {
+        overlay: false,
+      },
+      watchOptions: {
+        ignored: /node_modules/,
+      },
+    },
+    allowedHosts: 'all',
   },
 })
