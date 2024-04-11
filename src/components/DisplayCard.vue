88994a17e4a3a0e06fbe5c4b09d5a237c1c842e1
Dan Niculae <dan.niculae@logichainge.com>
2024-04-11 13:01:38 +0300

    small fixes

--- src/components/DisplayCard.vue
+++ src/components/DisplayCard.vue
@@ -1,14 +1,14 @@
 <template>
     <div>
-        <div class="aspect-small bg-amber-700 h-40 w-40 ml-2 mr-2 rounded-2xl relative" v-if="size == 's'">
+        <div class="aspect-small bg-amber-700 h-32 w-40 ml-1 mr-1 rounded-2xl relative" v-if="size == 's'">
             <img class="w-full h-full object-cover  rounded-2xl" :src="image" alt="">
             <h2 class="text-white font-bold text-lg absolute bottom-3 right-3 text-shadow">Some random text</h2>
         </div>
-        <div class="aspect-medium bg-amber-700 h-40 w-48 ml-2 mr-2 rounded-2xl relative" v-if="size == 'm'">
+        <div class="aspect-medium bg-amber-700 h-32 w-64 ml-1 mr-1 rounded-2xl relative" v-if="size == 'm'">
             <img class="w-full h-full object-cover  rounded-2xl" :src="image" alt="">
             <h2 class="text-white font-bold text-lg absolute bottom-3 right-3 text-shadow">Some random text</h2>
         </div>
-        <div class="aspect-large bg-amber-700 h-40 w-64 ml-2 mr-2 rounded-2xl relative" v-if="size == 'l'">
+        <div class="aspect-large bg-amber-700 h-32 w-88 ml-1 mr-1 rounded-2xl relative" v-if="size == 'l'">
             <img class="w-full h-full object-cover  rounded-2xl" :src="image" alt="">
             <h2 class="text-white font-bold text-lg absolute bottom-3 right-3 text-shadow">Some random text</h2>
         </div>
